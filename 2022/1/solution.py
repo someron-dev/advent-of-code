d67cbf64972942c0020f6724f5ba33ae9e7ab9e1
someron <devsomeron@gmail.com>
2022-12-03 11:25:08 +0100

    Added solution to Day 1 Part 2

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -1,16 +1,20 @@
 import sys
 
-with open(open(sys.argv[1])) as input_file:
+with open(sys.argv[1]) as input_file:
     elves = input_file.read().split("\n\n")
     
-    top = 0
+    all_elves = set()
 
     for data in elves:
         total = 0
         for line in data.strip().split("\n"):
             total += int(line)
 
-        if total > top:
-            top = total
+        all_elves.add(total)
 
-print("The Elf with the most calories is carrying", top, "cals.")
+sorted_elves = sorted(all_elves)
+print("Elve 1:", sorted_elves[-1])
+print("Elve 2:", sorted_elves[-2])
+print("Elve 3:", sorted_elves[-3])
+print(sorted_elves[-3:])
+print("Total:", sum(sorted_elves[-3:]))
